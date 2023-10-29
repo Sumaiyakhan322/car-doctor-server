6bc42f51fa08693767a31a522e624f7c9bd3f2f2
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2023-10-30 01:08:38 +0600

    added query to load the login users bokkings

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app=express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const port =process.env.PORT  || 5000
+const port =process.env.PORT  || 5001
 //middle ware
 app.use(cors())
 app.use(express.json())
@@ -25,16 +25,57 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
 
 
     const serviceCollection= client.db('carDoctor').collection('services')
+    const BookingCollection= client.db('carDoctor').collection('booking')
     app.get('/services',async(req,res)=>{
         const cursor=serviceCollection.find()
         const result=await cursor.toArray()
         res.send(result)
 
     })
+
+    app.get('/services/:id',async(req,res)=>{
+        const id=req.params.id
+        const query={_id:new ObjectId(id)}
+        const options = {
+            // Sort returned documents in ascending order by title (A->Z)
+            sort: { title: 1 },
+            // Include only the `title` and `imdb` fields in each returned document
+            projection: {  title: 1, price: 1,service_id:1 ,img:1},
+          };
+        const result=await serviceCollection.findOne(query,options)
+        res.send(result)
+    })
+
+
+    //bookings
+    app.post('/bookings',async(req,res)=>{
+        const booking=req.body;
+        const result=await BookingCollection.insertOne(booking)
+        res.send(result)
+    })
+    //use query
+    app.get('/bookings',async(req,res)=>{
+      console.log(req.query.customerEmail);
+      let query={}
+      if(req.query?.customerEmail){
+        query={customerEmail:req.query.customerEmail}
+      }
+        const cursor=BookingCollection.find(query)
+      
+        const result=await cursor.toArray()
+        
+        res.send(result)
+
+
+    })
+
+   
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
